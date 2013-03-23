c4403648e0ac84dbaf4020c741eecf4959951491
Crunchify <Crunchify@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-23 16:10:21 +0000

    Copyright Year Change.
    git-svn-id: https://plugins.svn.wordpress.org/foursquare-checkins/trunk@686295 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/setting-page/4sq-footer.php
+++ includes/setting-page/4sq-footer.php
@@ -9,6 +9,6 @@
 
 <div class="clear">
     <p>
-        <br/>&copy; Copyright 2007 - <?php echo date("Y"); ?> <a href="http://Crunchify.co" target="_blank">Crunchify.co</a>
+        <br/>&copy; Copyright 2012 - <?php echo date("Y"); ?> <a href="http://Crunchify.co" target="_blank">Crunchify.co</a>
     </p>
 </div>		
