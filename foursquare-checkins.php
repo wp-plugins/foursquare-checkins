2222bd48b3ea6043554046268315beb27b289dde
Crunchify <Crunchify@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-23 15:55:14 +0000

    = 1.1 = * Bug Fixes * Fixed ReadMe Typo * DomainName Change * Author's Twitter link fixed * HTML tag placement code now clearly visibles to editors
    git-svn-id: https://plugins.svn.wordpress.org/foursquare-checkins/trunk@686291 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- foursquare-checkins.php
+++ foursquare-checkins.php
@@ -3,7 +3,7 @@
 Plugin Name: FourSquare Checkins
 Plugin URI: http://crunchify.co/foursquare-checkins/
 Description: FourSquare and Wordpress Integration Plugin. Include/Show your FourSquare Checkins along with your Post/Page or in Blog's Sidebar.
-Version: 1.0
+Version: 1.1
 Author: Crunchify
 Author URI: http://crunchify.co
 */
