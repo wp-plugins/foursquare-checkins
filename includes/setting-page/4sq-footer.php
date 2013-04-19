84de98e2d83c67ad9039eb0cc65c3fbb55a2bf38
Crunchify <Crunchify@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-19 22:33:11 +0000

    Fixed a cross scripting security issue (props Charlie Eriksen via Secunia SVCRP)
    git-svn-id: https://plugins.svn.wordpress.org/foursquare-checkins/tags/1.3@700505 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/setting-page/4sq-footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Crunchify.com
+ * Plugin: Foursquare Checkins
+ */
+?>
+
+
+
+<div class="clear">
+    <p>
+        <br/>&copy; Copyright 2012 - <?php echo date("Y"); ?> <a href="http://Crunchify.com" target="_blank">Crunchify.com</a>
+    </p>
+</div>		
