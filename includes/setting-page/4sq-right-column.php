35be9a8d3a9d899eeced13745a08f48072ed37e0
Crunchify <Crunchify@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-09 16:58:55 +0000

    v1.5 - bug fixes and WordPress 3.9.1 compatibility
    git-svn-id: https://plugins.svn.wordpress.org/foursquare-checkins/tags/1.5@911184 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/setting-page/4sq-right-column.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Crunchify.com
+ * Plugin: Foursquare Checkins
+ */
+?>
+
+<?php
+require_once '4sq-right-homepage.php';
+require_once '4sq-right-otherplugins.php';
+?>
---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+jQuery.noConflict();
+
+jQuery(function () {
+    jQuery("#4sqtabs").tabs();
+});
+
+jQuery(function() {
+	jQuery('#4sqeast1').powerTip({placement: 'n'});
+});
